9d683e0b24d9fa5bcd9fe91e05a6a3470edd3c60
ermjonertas <ermert.jonas@yahoo.de>
2024-01-18 21:22:33 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'quiz.dart';
+
+void main() {
+  runApp(const Quiz());
+}
---
+++ lib/questions_summary/questions_summary.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import 'summary_item.dart';
+
+
+class QuestionsSummary extends StatelessWidget {
+  const QuestionsSummary(this.summaryData, {super.key});
+
+  final List<Map<String, Object>> summaryData;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 400,
+      child: SingleChildScrollView(
+        child: Column(
+          children: summaryData.map(
+            (data) {
+              return SummaryItem(data);
+            },
+          ).toList(),
+        ),
+      ),
+    );
+  }
+}
