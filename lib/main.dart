7f90d211ca35ff04f42dafd7a7a023b61fa0f607
Jonas <ermert.jonas@yahoo.de>
2026-09-23 13:53:07 +0200

    Ergänze einen umschaltbaren Hell- und Dunkelmodus

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,40 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
-import 'package:flutterquizapp/quiz_widgets.dart';
 
+import 'app_theme.dart';
 import 'models.dart';
+import 'quiz_widgets.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final quiz = await QuizLoader.loadFromBundle('assets/questions.json');
-  log(quiz.toString());
   runApp(MainApp(quiz: quiz));
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key, required this.quiz});
+
   final Quiz quiz;
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  ThemeMode mode = ThemeMode.system;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Flutter Quiz',
       debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.dark,
-      theme: ThemeData.dark(),
-      home: QuizPage(quiz: quiz),
+      theme: AppTheme.light,
+      darkTheme: AppTheme.dark,
+      themeMode: mode,
+      home: QuizPage(
+        quiz: widget.quiz,
+        mode: mode,
+        onThemeChanged: (value) => setState(() => mode = value),
+      ),
     );
   }
 }
